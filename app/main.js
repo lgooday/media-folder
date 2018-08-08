9b3be8c3ff1e32e956977572f2baa30f8eb64d9e
lgooday <36068283+lgooday@users.noreply.github.com>
2018-08-08 15:41:34 +0200

    fix .ext for backup

--- app/main.js
+++ app/main.js
@@ -1,62 +1,60 @@
-import nconf from './config'
-import _ from 'lodash'
+import assert from 'assert'
 import console from 'better-console'
 import async from 'async'
 import moment from 'moment'
-import * as db from './db'
-import * as dumper from './dump'
-import assert from 'assert'
 import commandLineArgs from 'command-line-args'
+import db from 'oz-mysql'
 import * as folder from './fold'
+import nconf from './config'
+import dumper from './dump'
 
 const cla = commandLineArgs([
-    { name: 'dump', type: String }
+	{ name: 'dump', type: String }
 ])
 
-main();
-
 function main() {
+	assert(nconf.get('mysql'), 'missing db')
+	assert(nconf.get('dir'), 'missing dir')
+	assert(nconf.get('batch'), 'missing batch')
 
-    assert(nconf.get('mysql'), 'missing db')
-    assert(nconf.get('dir'), 'missing dir')
-    assert(nconf.get('batch'), 'missing batch')
+	console.info('------------------------------------------------------------------')
+	console.info(moment.utc().format())
+	console.info(nconf.get('mysql').host + ' / ' + nconf.get('mysql').database)
+	console.info(nconf.get('dir').input)
+	console.info('concurrency : ' + nconf.get('batch').concurrency)
+	console.info('------------------------------------------------------------------')
 
-    console.info('------------------------------------------------------------------')
-    console.info(moment.utc().format())
-    console.info(nconf.get('mysql').host + ' / ' + nconf.get('mysql').database)
-    console.info(nconf.get('dir').input)
-    console.info('concurrency : ' + nconf.get('batch').concurrency)
-    console.info('------------------------------------------------------------------')
+	db.init(nconf.get('mysql'))
 
-    if (cla && cla.dump) {
+	if (cla && cla.dump) {
+		const date = moment(cla.dump, 'YYYY-MM-DD', true)
 
-        let date = moment(cla.dump, "YYYY-MM-DD", true);
+		if (date.isValid()) {
+			const dumpq = async.queue((date, next) => {
+				dumper.dump(date)
+					.then(next)
+					.catch(console.error)
+			}, 1)
 
-        if (date.isValid()) {
+			dumpq.drain = () => {
+				console.log('finished.')
+			}
 
-            let dumpq = async.queue((date, next) => {
-                dump(date)
-                    .then(next)
-                    .catch(console.error)
-            }, 1);
-
-            dumpq.drain = () => { console.log('finished.') };
-
-            while (date <= moment()) {
-                dumpq.push(date.clone());
-                date.add(1, 'day');
-            }
-
-        } else {
-            console.error('not a valid date');
-        }
-
-    } else {
-        folder.getFilesInFolder()
-            .then(folder.processFiles)
-            .then(dumper.dump)
-            .then(db.close)
-            .then(() => console.log('finished.'))
-            .catch(console.error)
-    }
+			while (date <= moment()) {
+				dumpq.push(date.clone())
+				date.add(1, 'day')
+			}
+		} else {
+			console.error('not a valid date')
+		}
+	} else {
+		folder.getFilesInFolder()
+			.then(folder.processFiles)
+			.then(dumper.dump)
+			.then(db.close)
+			.then(() => console.log('finished.'))
+			.catch(console.error)
+	}
 }
+
+main()
--- tests/fold.spec.js
+++ tests/fold.spec.js
@@ -1,18 +1,11 @@
-import nconf from '../app/config'
 import { expect } from 'chai'
-import sinon from 'sinon'
-import { t } from '../app/fold'
 import * as folder from '../app/fold'
 
-import "babel-polyfill"
+import 'babel-polyfill'
 
-describe('test main', () => {
-
-    describe('fold.js', () => {
-        it('should glob files in ./resources', async() => {
-            let files = await folder.getFilesInFolder()
-            expect(files).to.have.lengthOf(6)
-        })
-    })
-
+describe('fold.js', () => {
+	it('should glob files in ./resources', async () => {
+		const files = await folder.getFilesInFolder()
+		expect(files).to.have.lengthOf(6)
+	})
 })
